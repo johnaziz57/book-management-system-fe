a3dc7fab73072a94462ddc2bc51c92490eaf3f19
John <john_aziz57@yahoo.com>
2024-07-11 13:22:14 +0200

    Fix prettier errors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+
+    <div class="mb-3">
+      <label for="" class="from-label"> Page Title </label>
+      <input type="text" class="form-control" v-model="page.pageTitle" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Options({
   components: {
