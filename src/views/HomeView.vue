e5a35ec918ea71fef3057a8a717c2d345d00d131
John <john_aziz57@yahoo.com>
2024-07-13 21:01:15 +0200

    Format code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,7 @@
     <div class="container">
       <div class="row">
         <book-item
-          v-for="(book, index) in books"
+          v-for="(book, _) in books"
           :key="book.id"
           class="col-md-4"
           :book-id="book.id"
@@ -21,15 +21,15 @@
 <script lang="ts" setup>
 import BookItem from '@/components/BookItem.vue'
 import { BookStore } from '@/store/data'
-import { inject, watch, ref, reactive } from 'vue'
+import { inject, watch, ref } from 'vue'
 
 const $books = inject('$books') as BookStore
 
 let searchTerm = ref('')
-let books = reactive($books.getBooks())
+let books = $books.getBooks()
 
 watch(searchTerm, (newSearchTerm: string, _: string) => {
-  if (newSearchTerm){
+  if (newSearchTerm) {
     const response = $books.getBookByTitle(newSearchTerm)
     console.log(JSON.stringify(response))
     books = response
@@ -37,6 +37,5 @@ watch(searchTerm, (newSearchTerm: string, _: string) => {
     books = $books.getBooks()
   }
 })
-
 </script>
 <style lang="css" scoped></style>
