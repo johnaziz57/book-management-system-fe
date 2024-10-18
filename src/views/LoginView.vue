2b9beb99476ff7638b423bd58dc769cebe8263d3
John <john_aziz57@yahoo.com>
2024-10-18 14:59:34 +0200

    Fix closing the login modal

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- <div class="modal-backdrop"> -->
-    <div
+  <div
     class="modal"
-    id="loginModal" 
-    style="display: flex; background-color: #2125297B;"
+    id="loginModal"
+    style="display: flex; background-color: #2125297b"
     tabindex="-1"
     aria-labelledby="modalLabel"
-    @click.prevent="closeModal"
+    @click.self.prevent="closeModal"
   >
     <div class="modal-dialog">
       <div class="modal-content">
@@ -17,14 +17,18 @@
             class="btn-close"
             data-bs-dismiss="modal"
             aria-label="Close"
-            @click="closeModal"
+            @click.prevent="closeModal"
           ></button>
         </div>
         <div class="modal-body">
           <p>This is a simple modal built with Bootstrap and Vue 3.</p>
+          <div class="inputWithLabel">
+            <label for="" class="label">Title</label>
+            <input type="text" class="form-control" v-model="title" />
+          </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" @click="closeModal">
+          <button type="button" class="btn btn-secondary" @click.prevent="closeModal">
             Close
           </button>
         </div>
@@ -36,11 +40,11 @@
 <script setup>
 import { defineProps } from 'vue'
 
-const { onClose } = defineProps({onClose: Function})
+const { onClose } = defineProps({ onClose: Function })
 
-const closeModal =() => {
-  console.log("emit close");
-  
+const closeModal = () => {
+  console.log('closeModal')
+
   onClose()
 }
 
